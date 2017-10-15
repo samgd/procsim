4671ace92443bbbb12a62ac72274e865e17d2fd1
samgd <sam@samgd.com>
2017-10-15 14:43:07 +0100

    Result equality for testing

--- procsim/back_end/result.py
+++ procsim/back_end/result.py
@@ -4,8 +4,16 @@ class Result:
     Args:
         dest: Name of Register to write value in.
         value: Value to write in dest.
+
+    Attributes:
+        dest: Name of Register to write value in.
+        value: Value to write in dest.
     """
 
     def __init__(self, dest, value):
         self.dest = dest
         self.value = value
+
+    def __eq__(self, other):
+        """Results are equal if they have equal dest and value attributes."""
+        return self.dest == other.dest and self.value == other.value
