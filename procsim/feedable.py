0b6e8645863207cc189ba0ea04e20bd094996ea7
samgd <sam@samgd.com>
2017-10-15 14:38:32 +0100

    Add food argument to Feedable

--- procsim/feedable.py
+++ procsim/feedable.py
@@ -3,7 +3,7 @@ import abc
 class Feedable(abc.ABC):
     """A Feedable instance can be fed."""
     @abc.abstractmethod
-    def feed(self):
+    def feed(self, food):
         pass
 
     @abc.abstractmethod
