e066ad3146f93e84b8861e9df4de02a8bdb222c6
samgd <sam@samgd.com>
2017-10-19 16:39:39 +0100

    ReservationStation single execution unit

--- procsim/back_end/reservation_station.py
+++ procsim/back_end/reservation_station.py
@@ -1,4 +1,7 @@
 from collections import defaultdict
+from copy import copy
+from itertools import filterfalse
+import inspect
 
 from procsim.back_end.execution_unit import ExecutionUnit
 from procsim.clocked import Clocked
@@ -36,12 +39,31 @@ class ReservationStation(Clocked, Feedable):
         return len(self.future_buffer) == self.CAPACITY
 
     def operate(self):
-        """Issue fed Instructions to capable and non-busy ExecutionUnits."""
-        pass
+        """Issue fed Instructions to capable and non-busy ExecutionUnits.
+
+        Raises:
+            AssertionError if no ExecutionUnits exist that are capable of
+            executing the Instruction.
+        """
+        for instruction in self.current_buffer:
+            units_exist = False   # If no units exist for instruction, error.
+            # Iterate from most to least specific capability.
+            for capability in inspect.getmro(type(instruction)):
+                units = self.execution_units[capability]
+                units_exist |= len(units) > 0
+                free_units = {unit for unit in units if not unit.busy()}
+                if len(free_units) < 1:
+                    continue
+                unit = next(iter(free_units))
+                unit.feed(instruction)
+                self.future_buffer.remove(instruction)
+                break
+            assert units_exist, 'Instruction %r has no ExecutionUnit' % instruction
 
     def trigger(self):
         """Free up buffer space by removing the issued Instructions."""
-        pass
+        self.current_buffer = self.future_buffer
+        self.future_buffer = copy(self.current_buffer)
 
     def register(self, execution_unit):
         """Enable the ReservationStation to issue Instructions to the unit.
--- test/back_end/test_reservation_station.py
+++ test/back_end/test_reservation_station.py
@@ -1,10 +1,21 @@
 import random
 import unittest
 
-from procsim.instructions import Add
 from procsim.back_end.reservation_station import ReservationStation
+from procsim.back_end.execution_unit import ExecutionUnit
+from procsim.clock import Clock
+from procsim.instructions import Add
+from procsim.instructions import AddI
+from procsim.instructions import Instruction
 
 class TestReservationStation(unittest.TestCase):
+
+    def setUp(self):
+        self.clock = Clock()
+        self.rs = ReservationStation(capacity=200)
+        self.iu = IntegerUnitStub()
+        self.clock.register(self.rs)
+        self.clock.register(self.iu)
 
     def test_invalid_capacity(self):
         """Test exception thrown when initialized with invalid capacity."""
@@ -25,3 +36,91 @@ class TestReservationStation(unittest.TestCase):
                             'ReservationStation should be busy after capacity feeds')
             with self.assertRaises(AssertionError):
                 rs.feed(Add('r1', 'r2', 'r3'))
+
+    def test_tick_no_execution_units(self):
+        """Ensure error raised when no ExecutionUnits exist for an Instruction."""
+        # Feed Instruction and tick to move it from future to current state.
+        self.rs.feed(Add('r0', 'r1', 'r2'))
+        self.rs.tick()
+        with self.assertRaises(AssertionError):
+            self.rs.tick()
+
+    def test_instruction_passthrough_one_feed_one_execution(self):
+        """Test one Instruction passes through ReservationStation OK."""
+        self.rs.register(self.iu)
+
+        instruction = AddI('r0', 'r1', 10)
+        self.rs.feed(instruction)
+        self.clock.tick()
+        self.assertSetEqual(self.iu.received, set())
+
+        self.clock.tick()
+        self.assertSetEqual(self.iu.received, set([instruction]))
+
+    def test_instruction_passthrough_sequential_feed_one_execution(self):
+        """Test sequential Instruction feeds pass through ReservationStation OK."""
+        self.rs.register(self.iu)
+
+        fed_instructions = set()
+        last_feed = None
+        for i in range(100):
+            # Skip feeding occasionally to insert bubbles into the pipeline.
+            if not i % 7 == 0:
+                last_feed = AddI('r0', 'r1', i)
+                self.rs.feed(last_feed)
+                fed_instructions.add(last_feed)
+            else:
+                last_feed = None
+            self.clock.tick()
+            # ReservationStation has a 1 cycle delay - not expecting to receive
+            # last instruction fed out until next cycle.
+            exp_received = fed_instructions - set([last_feed])
+            self.assertEqual(self.iu.received, exp_received)
+
+        self.clock.tick()
+        self.assertEqual(self.iu.received, fed_instructions)
+
+    def test_instruction_passthrough_many_feed_one_exeuction(self):
+        """Test many Instructions fed first then pass through OK."""
+        self.rs.register(self.iu)
+
+        num_inst = 100
+        fed_instructions = set()
+        for i in range(num_inst):
+            instruction = AddI('r0', 'r1', i)
+            self.rs.feed(instruction)
+            fed_instructions.add(instruction)
+        self.clock.tick()
+
+        for i in range(num_inst):
+            self.clock.tick()
+        self.assertEqual(self.iu.received, fed_instructions)
+
+class IntegerUnitStub(ExecutionUnit):
+    """Stub that logs instructions fed. Can receive 1 Instruction per tick.
+
+    Attributes:
+        received: Set containing all Instructions that have been arguments to
+        feed calls.
+    """
+
+    def __init__(self):
+        self.received = set()
+        self.fed = False
+
+    def capability(self):
+        return Instruction
+
+    def feed(self, instruction):
+        self.received.add(instruction)
+        self.fed = True
+
+    def busy(self):
+        return self.fed
+
+    def operate(self):
+        pass
+
+    def trigger(self):
+        self.fed = False
+
