fff18cddef1504c4541154b16e6588936eefee04
samgd <sam@samgd.com>
2017-10-22 14:08:29 +0100

    Front-end integration only needs to check feed_log

--- test/front_end/test_fetch_decode_integration.py
+++ test/front_end/test_fetch_decode_integration.py
@@ -16,7 +16,7 @@ TEST_PROGRAM = [ins.Add('r1', 'r2', 'r3'),
 
 class TestFetchDecodeIntegration(unittest.TestCase):
 
-    def init_trace(self):
+    def init_run(self):
         self.program_file = make_program_file(TEST_PROGRAM)
 
         self.feed_log = FeedLog()
@@ -27,18 +27,16 @@ class TestFetchDecodeIntegration(unittest.TestCase):
                            self.program_file,
                            self.decode)
 
-    def cleanup_trace(self):
+    def cleanup_run(self):
         os.remove(self.program_file)
 
     def test_fetch_decode_integration(self):
-        """Ensure different operate and trigger orders have equal outputs."""
-        traces = {'run_0': [], 'run_1': []}
+        """Ensure different operate and trigger both produce the TEST_PROGRAM."""
 
-        for run in traces:
-            self.init_trace()
+        for run in range(2):
+            self.init_run()
 
-            traces[run].append(deepcopy(self.reg_file))
-            for _ in range(10):
+            for _ in range(15):
                 if run == 'run_0':
                     self.decode.operate()
                     self.fetch.operate()
@@ -49,9 +47,6 @@ class TestFetchDecodeIntegration(unittest.TestCase):
                     self.decode.operate()
                     self.fetch.trigger()
                     self.decode.trigger()
-                traces[run].append(deepcopy(self.reg_file))
 
-            self.cleanup_trace()
-
-        self.assertEqual(traces['run_0'], traces['run_1'])
-        self.assertEqual(self.feed_log.log, TEST_PROGRAM)
+            self.assertEqual(self.feed_log.log, TEST_PROGRAM)
+            self.cleanup_run()
