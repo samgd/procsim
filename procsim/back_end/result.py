095a2cd0cc6d73c1337025c0c67c6909c663fb8c
samgd <sam@samgd.com>
2017-10-24 18:52:42 +0100

    Allow Results to write to Memory

--- procsim/back_end/result.py
+++ procsim/back_end/result.py
@@ -1,18 +1,27 @@
+from procsim.register import Register
+
 class Result:
     """Result of executing an Instruction.
 
     Args:
-        dest: Name of Register to write value in.
+        dest: Register or Memory address to write value in, depending on the
+            Result typ.
         value: Value to write in dest.
+        typ: Result type. Specifies the semantic meaning of dest.
+            (default Register)
 
     Attributes:
-        dest: Name of Register to write value in.
+        dest: Register or Memory address to write value in, depending on the
+            Result typ.
         value: Value to write in dest.
+        typ: Result type. Specifies the semantic meaning of dest.
+            (default Register)
     """
 
-    def __init__(self, dest, value):
+    def __init__(self, dest, value, typ=Register):
         self.dest = dest
         self.value = value
+        self.typ = typ
 
     def __eq__(self, other):
         """Results are equal if they have equal dest and value attributes."""
@@ -21,4 +30,4 @@ class Result:
         return False
 
     def __repr__(self):
-        return 'Result(%r, %r)' % (self.dest, self.value)
+        return 'Result(%r, %r, %s)' % (self.dest, self.value, self.typ.__name__)
