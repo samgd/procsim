0e66d8df373f01e4a1a7e110bd38de089e0911da
samgd <sam@samgd.com>
2017-10-25 11:39:08 +0100

    Additional programs

--- programs/vector_addition.py
+++ programs/vector_addition.py
@@ -2,7 +2,7 @@ from procsim.register_file import RegisterFile
 from procsim.memory import Memory
 
 LEN_ARRAY = 50
-MEMORY = Memory(LEN_ARRAY * 2)
+MEMORY = Memory(LEN_ARRAY * 3)
 # Initialize two arrays with values [0...LEN_ARRAY - 1]
 for array_idx in range(2):
     for elem_idx in range(LEN_ARRAY):
@@ -16,13 +16,13 @@ REGISTER_FILE = RegisterFile(6)
 # r2 holds absolute elem_idxs (relative + array_idx * LEN_ARRAY)
 # r3 holds value loaded from array 1 and the multiplication results
 # r4 holds value loaded from array 2
-# r5 holds the running sum
 PROGRAM = ['addi r0 r0 %d' % LEN_ARRAY,
            'ldr r3 r1',
            'add r2 r1 r0',
            'ldr r4 r2',
-           'mul r3 r3 r4',
-           'add r5 r5 r3',
+           'add r2 r2 r0',
+           'add r3 r3 r4',
+           'str r3 r2',
            'addi r1 r1 1',
            'addi r2 r1 0',
            'blth r1 r0 1']
